d45c15853e7e7271e3fa42d33db1e432fa1b042f
Chandan07Gowda <chandannallurkrishnappa@gmail.com>
2026-02-03 16:18:34 +0530

    AI security fixes: 4 vulnerabilities fixed, 1 require manual review
    Fixed rules:
    - javascript:S5852: Food:src/screens/Login.js
    - javascript:S5852: Food:src/screens/Login.js
    - javascript:S5852: Food:src/screens/RegisterRestaurant.js
    - javascript:S5852: Food:src/screens/RegisterRestaurant.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -90,7 +90,7 @@ export default class Login extends Component {
 
     handleUserPassword(e) {
         const userPassword = e;
-        const userPasswordFormate = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}/;
+        const userPasswordFormate = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}$/;
         if (userPassword.match(userPasswordFormate)) {
             this.setState({
                 showError: false,
@@ -224,7 +224,7 @@ export default class Login extends Component {
         // const whiteSpaces = /^(?!\s*$)[-a-zA-Z0-9_:,.' ']{1,100}$/;
         const userNameFormate = /^([A-Za-z.\s_-]).{5,}$/;
         const userEmailFormate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        const userPasswordFormate = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}/;
+        const userPasswordFormate = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}$/;
         const userCountryFormate = /^([A-Za-z.\s_-]).{5,}$/;
         const userCityFormate = /^([A-Za-z.\s_-]).{5,}$/;
 
